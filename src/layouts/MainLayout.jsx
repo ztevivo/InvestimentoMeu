376fdd468699c97e6db87dd5dda878620e777f3c
ztevivo <104692280+ztevivo@users.noreply.github.com>
2026-06-16 13:17:40 -0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,18 +3,17 @@ import Header from "../components/Header";
 import Dashboard from "../pages/Dashboard";
 
 export default function MainLayout() {
-return (
+  return (
+    <div className="flex min-h-screen bg-slate-100">
+      <Sidebar />
 
+      <div className="flex-1">
+        <Header />
 
-
-
-  <div className="flex-1">
-    <Header />
-
-    <main className="p-6">
-      <Dashboard />
-    </main>
-  </div>
-</div>
-
-);
+        <main className="p-6">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+}
