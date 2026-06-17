903864ebcf70dc6525487445fc46bf0c3e631736
ztevivo <104692280+ztevivo@users.noreply.github.com>
2026-06-17 09:02:49 -0300

    Add files via upload

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import Dashboard from "../pages/Dashboard";
+
+export default function MainLayout() {
+  return (
+    <div className="flex min-h-screen bg-slate-100">
+      <Sidebar />
+
+      <div className="flex-1">
+        <Header />
+
+        <main className="p-6">
+          <Dashboard />
+        </main>
+      </div>
+    </div>
+  );
+}
