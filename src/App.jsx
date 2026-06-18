02517c3f2eb24c4f67abeb7a4fb5c854a379c2bf
ztevivo <104692280+ztevivo@users.noreply.github.com>
2026-06-18 08:39:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { AppProvider, useApp } from "./context/AppContext";
 import MainLayout from "./layouts/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Carteiras from "./pages/Carteiras";
+import Ativos from "./pages/Ativos";
+import Operacoes from "./pages/Operacoes";
 
-function App() {
-  return <MainLayout />;
+function PageRouter() {
+  const { currentPage } = useApp();
+  const pages = { dashboard: <Dashboard />, carteiras: <Carteiras />, ativos: <Ativos />, operacoes: <Operacoes /> };
+  return <MainLayout>{pages[currentPage] || <Dashboard />}</MainLayout>;
 }
 
-export default App;
+export default function App() {
+  return <AppProvider><PageRouter /></AppProvider>;
+}
